f610d49cfb982ae56b23111a17710f84b60ae2ce
shashankv05 <shashank.verma541@gmail.com>
2021-08-21 18:05:58 +0530

    Commited Stack.java

---
+++ Stack.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+// Student class is accessible by default 
+class StackDemo {
+
+    public static void main(String[] args) {
+    
+    Stack <Student> s = new Stack<>();
+    s.push(new Student(5, "Shashank"));
+    s.push(new Student(4, "Ishaan"));
+    s.push(new Student(21, "Simranjeet"));
+    s.push(new Student(42, "Ranveer"));
+    System.out.println(s.peek());
+    
+    s.pop();
+
+    System.out.println(s);
+    }    
+
+}
